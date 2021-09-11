3b66501b9b0cd88cbbd386727acf1d9a5fca9109
Joseph <joe@uhhuhohyeah.com>
2021-09-11 15:09:13 -0700

    added more stuff to this incomprehensible buggy lesson

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,13 @@
 import React, { useState } from 'react';
+import Auth from '../utils/auth';
+
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 
 const Login = (props) => {
   const [formState, setFormState] = useState({ email: '', password: '' });
+
+  const [login, { error }] = useMutation(LOGIN_USER);
 
   // update state based on form input changes
   const handleChange = (event) => {
@@ -14,15 +20,30 @@ const Login = (props) => {
   };
 
   // submit form
-  const handleFormSubmit = async (event) => {
+  const handleFormSubmit = async event => {
     event.preventDefault();
+
+    try {
+      const { data } = await login({
+        variables: { ...formState }
+      });
+    
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
     // clear form values
     setFormState({
       email: '',
       password: '',
     });
-  };
+  
+
+
+
+
 
   return (
     <main className='flex-row justify-center mb-4'>
@@ -53,6 +74,7 @@ const Login = (props) => {
                 Submit
               </button>
             </form>
+            {error && <div>Login failed</div>}
           </div>
         </div>
       </div>
